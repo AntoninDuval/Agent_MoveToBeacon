8f2c53a09a3d529f40432914f4c9ff3f8615a636
AntoninDuval <antonin.duval@hotmail.fr>
2019-03-03 14:13:44 +0100

    Updated agentBeacon
    Updated the code so that it match version 2.0.1 of PySC2 API.

--- AgentBeacon.py
+++ AgentBeacon.py
@@ -6,9 +6,10 @@ import numpy as np
 import pandas as pd
 
 from pysc2.agents import base_agent
-from pysc2.lib import actions
-from pysc2.lib import features
+from pysc2.env import sc2_env
+from pysc2.lib import actions, features, units
 
+from absl import app
 import time
 
 _PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
@@ -30,10 +31,10 @@ DATA_FILE = 'sparse_agent_data'
 
 smart_actions = []
 
-for mm_x in range(0, 80):
-    for mm_y in range(0, 64):
-        if (mm_x + 1) % 16 == 0 and (mm_y + 1) % 16 == 0:
-            smart_actions.append(ACTION_ATTACK + '_' + str(mm_x - 8) + '_' + str(mm_y - 8))
+for mm_x in range(1, 80):
+    for mm_y in range(1, 64):
+        if mm_x % 8 == 0 and mm_y % 8 == 0:
+            smart_actions.append(ACTION_ATTACK + '_' + str(mm_x - 4) + '_' + str(mm_y - 4))
 
 
 
@@ -44,7 +45,7 @@ class QLearningTable:
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon = e_greedy
-        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+        self.q_table = pd.read_pickle('sparse_agent_data')
 
     def choose_action(self, observation):
         self.check_state_exist(observation)
@@ -68,8 +69,7 @@ class QLearningTable:
         self.check_state_exist(s)
         
         q_predict = self.q_table.ix[s, a]
-        
-        q_target = r  # next state is terminal
+        q_target = r + self.gamma * self.q_table.ix[s_, :].max()
             
         # update
         self.q_table.ix[s, a] += self.lr * (q_target - q_predict)
@@ -78,116 +78,170 @@ class QLearningTable:
         if state not in self.q_table.index:
             # append new state to q table
             self.q_table = self.q_table.append(pd.Series([0] * len(self.actions), index=self.q_table.columns, name=state))
+            
+    def print_q_table(self, s, a):
+        total_rows=self.q_table.count
+        q_predict = self.q_table.ix[s, a]
+        print('action : ',a,' q_value : ',q_predict)
+            
 
 
 class MoveToBeacon(base_agent.BaseAgent):
     def __init__(self):
-        
-        super(MoveToBeacon, self).__init__()
- 
-        self.qlearn = QLearningTable(actions=list(range(len(smart_actions))))
-        self.previous_action = None
-        self.previous_state = None
-        self.previous_reward = 0
-        
-
-    def splitAction(self, action_id):
-        smart_action = smart_actions[action_id]
+            super(MoveToBeacon, self).__init__()
             
-        x = 0
-        y = 0
-        if '_' in smart_action:
-            smart_action, x, y = smart_action.split('_')
-
-        return (smart_action, x, y)
-
+            
+            self.resultat = []
+            self.episode = 0
+            
+     
+            self.qlearn = QLearningTable(actions=list(range(len(smart_actions))))
+            self.previous_action = None
+            self.previous_state = None
+            self.previous_reward = 0
+            self.destination_reached = False
+            self.pos_marine = []
+            
+    def unit_type_is_selected(self, obs, unit_type):
+        if (len(obs.observation.single_select) > 0 and
+            obs.observation.single_select[0].unit_type == unit_type):
+          return True        
     
+    def get_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.feature_units
+                if unit.unit_type == unit_type]
+    
+    def splitAction(self, action_id):
+            smart_action = smart_actions[action_id]
+                
+            x = 0
+            y = 0
+            if '_' in smart_action:
+                smart_action, x, y = smart_action.split('_')
+    
+            return (smart_action, x, y)
+    
+        
     def step(self, obs):
-        
-        super(MoveToBeacon, self).step(obs)
-        time.sleep(0.2)
-        
-        if obs.first():
-            self.previous_action = None
-            self.previous_state = None
             
-        
-        current_state= np.zeros(16)
-        hot_squares = np.zeros(16)
-        
-        beacon_y, beacon_x = (obs.observation["screen"][_PLAYER_RELATIVE] == _PLAYER_NEUTRAL).nonzero()
-
-        
-
-        
-        for i in range(0, len(beacon_y)):
-            y = int(math.ceil((beacon_y[i] + 1) / 16))
-            x = int(math.ceil((beacon_y[i] + 1) / 16))
+            super(MoveToBeacon, self).step(obs)
             
+            time.sleep(0.5)
+            if obs.first():
+                self.previous_action = None
+    
+            if obs.last():
+                score = obs.observation["score_cumulative"][0]
+                self.resultat.append(score)
+                self.episode += 1
+                if self.episode%20==0:
+                    df = pd.Series(self.resultat)
+                    df.to_csv(path='C:/Users/Antonin/Agent/score_newversion.csv' ,sep=';',index=True)
+                    
+               
+            current_state= np.zeros(275)
+            beacon_y, beacon_x = (obs.observation.feature_minimap.player_relative == features.PlayerRelative.NEUTRAL).nonzero()
+    
             
-            hot_squares[((y - 1) * 4) + (x - 1)] = 1
+            for i in range(0, (len(beacon_y)-1)):         
+                current_state[i] = beacon_y[i]
+            for j in range (0, (len(beacon_x)-1)):
+                current_state[j+137] = beacon_x[j]
+     
+    
+            if self.previous_reward != obs.observation["score_cumulative"][0] and self.previous_action is not None:
+                #if there was a change in the score and an action was done, we feed the Q-table with this action
+                reward = 1
+                self.qlearn.learn(str(self.previous_state), self.previous_action, reward,str(current_state))
+              
+            if self.previous_reward == obs.observation["score_cumulative"][0] and self.previous_action is not None and self.destination_reached:
+                #if the score doesn't change and the marine has reached his destination, we give a negative reward
+                reward = -2
+                self.qlearn.learn(str(self.previous_state), self.previous_action, reward,str(current_state))
+                
+    
+            self.previous_reward = obs.observation["score_cumulative"][0]
+    
+            #we don't change anything while the marine hasn't reached his destination
+            if self.previous_action !=None:
+                last_action, x, y = self.splitAction(self.previous_action)
+                target=[int(x),int(y)]
+                #print('target : ',x,',',y)
+                if last_action== ACTION_ATTACK:
+                    m_y, m_x = (obs.observation.feature_screen.player_relative == features.PlayerRelative.SELF).nonzero()
+                    if m_x.any():
+                        for i in range(0,len(m_x)):
+                            pos_marine = [m_x[i],m_y[i]]
+                            #print('pos_x(',i,'):',m_x[i],', pos_y(',i,'):',m_y[i])
+                            if target == pos_marine or len(m_x)<8:
+                                self.destination_reached = True
+                    if not self.destination_reached:
+                        return actions.FUNCTIONS.Attack_screen("now",target)
+                    
+                    
+                    
+    
+            marines = self.get_units_by_type(obs, units.Terran.Marine)
             
-        for i in range(0, 16):
-            current_state[i] = hot_squares[i]
+            marine = random.choice(marines)
 
-
-
-        if self.previous_reward != obs.observation["score_cumulative"][0] and self.previous_action is not None:
+            self.pos_marine = [marine.x,marine.y]
+            self.previous_pos_marine= self.pos_marine
+            if self.pos_marine != self.previous_pos_marine:
+                return actions.FUNCTIONS.Attack_screen("now",target)
             
-            reward = 1
-            self.qlearn.learn(str(self.previous_state), self.previous_action, reward,str(current_state))
+
+            rl_action = self.qlearn.choose_action(str(current_state)) #we let the q-table decide of the next action and add the state if it doesn't exist
+    
+    
+            self.qlearn.print_q_table(str(current_state),rl_action)
+       
+            self.previous_state = current_state
             
-            self.qlearn.q_table.to_pickle(DATA_FILE + '.gz', 'gzip')           
-            self.previous_action = None
-            self.previous_state = None
-        
+            self.previous_action = rl_action
+    
 
-        #if the marine hasn't reach the position of the previous action, we keep doing the same action
-
-        if self.previous_action !=None :
-            last_action, x, y = self.splitAction(self.previous_action)
+            smart_action, x, y = self.splitAction(self.previous_action)
             target=[int(x),int(y)]
-            if last_action== ACTION_ATTACK:
-                m_y, m_x = (obs.observation["screen"][_PLAYER_RELATIVE] == _PLAYER_FRIENDLY).nonzero()
-                if m_x.any():
-                    m_x = int(round(m_x.mean()))
-                    m_y = int(round(m_y.mean()))
-                    pos_marine = [m_x,m_y]
-                    print ('destination marine : ',x,',',y)
-                    print ('position marine : ',x,',',y)
+    
+            if smart_action == ACTION_ATTACK:
+                if _ATTACK_SCREEN in obs.observation['available_actions']:
+                    self.destination_reached = False
+                    return actions.FUNCTIONS.Attack_screen("now",target)
+                else: #select the marine if attack is not possible
+                    return actions.FUNCTIONS.select_army("select")
             
-
-                    if target != pos_marine:
-                        return actions.FunctionCall(_ATTACK_SCREEN, [_NOT_QUEUED, target])
             
-
-
-
-
-
+            return actions.FunctionCall(_NO_OP, [])
         
+    
+def main(unused_argv):
+          agent = MoveToBeacon()
+          try:
+            while True:
+              with sc2_env.SC2Env(
+                  map_name="MoveToBeacon",
+                  players=[sc2_env.Agent(sc2_env.Race.terran)],
+                  agent_interface_format=features.AgentInterfaceFormat(
+                      feature_dimensions=features.Dimensions(screen=84, minimap=64),
+                      use_feature_units=True),
+                  step_mul=16,
+                  game_steps_per_episode=0,
+                  visualize=True) as env:
+                  
+                agent.setup(env.observation_spec(), env.action_spec())
+                
+                timesteps = env.reset()
+                agent.reset()
+                
+                while True:
+                  step_actions = [agent.step(timesteps[0])]
+                  timesteps = env.step(step_actions)
+              
+          except KeyboardInterrupt:
+            pass
+      
+if __name__ == "__main__":
+    app.run(main)
 
-        rl_action = self.qlearn.choose_action(str(current_state))
-        self.previous_state = current_state
-        self.previous_action = rl_action
-        self.previous_reward = obs.observation["score_cumulative"][0]
-
-        smart_action, x, y = self.splitAction(self.previous_action)
-        target=[int(x),int(y)]
-
-        if smart_action == ACTION_ATTACK:
-            if _ATTACK_SCREEN in obs.observation['available_actions']:
-                return actions.FunctionCall(_ATTACK_SCREEN, [_NOT_QUEUED, target])
-            else:
-                return actions.FunctionCall(_SELECT_ARMY, [_NOT_QUEUED])
-        
-        
-        return actions.FunctionCall(_NO_OP, [])
-
-            
-        
- 
-
-
-
-
+         
